379090e1d4313a298a87ae01545d3e9c00d64d90
Pedro Henrique Pereira de Jesus <pjesus@meta.com>
2026-09-21 12:25:27 +0100

    Add tailwind-css support via dx

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,23 @@
 use dioxus::prelude::*;
 
+const TAILWIND_CSS: Asset = asset!("/assets/tailwind.css");
+
 fn main() {
     dioxus::launch(App);
 }
 
 #[component]
 fn App() -> Element {
+    // 1 is dark square, 0 is a light square
+    let board: [i32; 8] = [1, 0, 1, 0, 1, 0, 1, 0];
+
     rsx! {
-        h1 { "Hello, Dioxus!" }
-        h2 { "New header 2!" }
+        document::Stylesheet { href: TAILWIND_CSS }
+
+        h1 { class: "text-2xl font-bold", "Hello, Dioxus!" }
+
+        for i in board {
+            div { class: "w-16 h-16", "test {i}" }
+        }
     }
 }
